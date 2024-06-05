06183146b80aa909a8068cdc3a307fdc0ba8ede8
Jimmy Ramani <jimmy.ramani0912@gmail.com>
2024-06-05 19:02:22 +0530

    Implement email and password validation mechanisms

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,17 @@
-import { IsString, IsNotEmpty, MinLength } from 'class-validator';
-
+import { IsEmail, IsNotEmpty, Matches, MinLength } from 'class-validator';
 export class LoginDto {
-  @IsString()
+  @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @IsString()
   @IsNotEmpty()
   @MinLength(6)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+    {
+      message:
+        'password must contain at least one uppercase letter, one lowercase letter, one number and one special character',
+    },
+  )
   password: string;
 }
